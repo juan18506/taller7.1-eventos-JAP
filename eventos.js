ba57dfe314854238e01cf78a73656ba586428d32
juan18506 <juan185067@gmail.com>
2023-08-06 21:04:51 -0300

    solucionado la propagacion al hacer click en el boton

--- eventos.js
+++ eventos.js
@@ -1,4 +1,10 @@
-const div = document.getElementsByTagName('div')[0]
+const div = document.querySelector('.div')
+const button = div.querySelector('.btn')
+
+button.addEventListener('click', (event) => {
+  event.stopPropagation()
+  alert('Hola!')
+})
 
 div.addEventListener('click', () => {
   alert('Hola! Soy el div')
